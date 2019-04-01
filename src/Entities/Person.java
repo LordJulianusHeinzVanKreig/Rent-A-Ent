0df6e0113ff37763919c6c1f2dc66ff37e6ceb2f
VanMeier <meierdo.dm@gmail.com>
2019-04-01 16:17:14 +0200

    Person class implemented

--- src/Entities/Person.java
+++ src/Entities/Person.java
@@ -1,13 +1,59 @@
 package Entities;
 
-public class Person extends DatabaseEntity {
-	private String FirstName;
-
-	public Person(int id, String firstName) {
-		super(id);
-		this.FirstName = firstName;
-		// TODO Auto-generated constructor stub
-	}
-
+public abstract class Person extends DatabaseEntity {
 	
+	private String FirstName;
+	private String LastName;
+	private String HouseNumber;
+	private int ZipCode;
+	private String Street;
+	private String PhoneNumber;
+	
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getHouseNumber() {
+		return HouseNumber;
+	}
+	public void setHouseNumber(String houseNumber) {
+		HouseNumber = houseNumber;
+	}
+	public int getZipCode() {
+		return ZipCode;
+	}
+	public void setZipCode(int zipCode) {
+		ZipCode = zipCode;
+	}
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+	
+	public Person(int id, String firstName, String lastName, String houseNumber, int zipCode, String street,
+			String phoneNumber) {
+		super(id);
+		FirstName = firstName;
+		LastName = lastName;
+		HouseNumber = houseNumber;
+		ZipCode = zipCode;
+		Street = street;
+		PhoneNumber = phoneNumber;
+	}
 }
