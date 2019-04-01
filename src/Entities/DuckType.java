e0c0eb5375d777a9330b679bd4bcbe4fd4f6dc3d
Eresorm <meierdo.dm@gmail.com>
2019-04-01 15:48:58 +0200

    DuckType implemented

--- src/Entities/DuckType.java
+++ src/Entities/DuckType.java
@@ -1,10 +1,36 @@
 package Entities;
 
+import java.util.Date;
+
 public class DuckType extends DatabaseEntity {
-
-	public DuckType(int id) {
-		super(id);
-		// TODO Auto-generated constructor stub
+	
+	private Date BreedTime;
+	private String Region;
+	private int maxAge;
+	
+	public Date getBreedTime() {
+		return BreedTime;
 	}
-
+	public void setBreedTime(Date breedTime) {
+		BreedTime = breedTime;
+	}
+	public String getRegion() {
+		return Region;
+	}
+	public void setRegion(String region) {
+		Region = region;
+	}
+	public int getMaxAge() {
+		return maxAge;
+	}
+	public void setMaxAge(int maxAge) {
+		this.maxAge = maxAge;
+	}
+	
+	public DuckType(int id, Date breedTime, String region, int maxAge) {
+		super(id);
+		BreedTime = breedTime;
+		Region = region;
+		this.maxAge = maxAge;
+	}
 }
