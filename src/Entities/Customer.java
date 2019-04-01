0e82b27ebaf48bb0edec38d3bc6ed8d7e4d31040
VanMeier <meierdo.dm@gmail.com>
2019-04-01 16:23:58 +0200

    Customer class implemented

---
+++ src/Entities/Customer.java
@@ -0,0 +1,18 @@
+package Entities;
+
+public class Customer extends Person {
+	private int Rating;
+
+	public int getRating() {
+		return Rating;
+	}
+	public void setRating(int rating) {
+		Rating = rating;
+	}
+	
+	public Customer(int id, String firstName, String lastName, String houseNumber, int zipCode, String street,
+			String phoneNumber, int rating) {
+		super(id, firstName, lastName, houseNumber, zipCode, street, phoneNumber);
+		Rating = rating;
+	}
+}
