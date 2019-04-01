3797364b7b3ebc2c0f5e95c6f2b22e6c22e1a5e4
VanMeier <meierdo.dm@gmail.com>
2019-04-01 16:21:10 +0200

    Worker class implemented ☭

---
+++ src/Entities/Worker.java
@@ -0,0 +1,26 @@
+package Entities;
+
+public class Worker extends Person {
+	private int Age;
+	private int WorkHours;
+	
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public int getWorkHours() {
+		return WorkHours;
+	}
+	public void setWorkHours(int workHours) {
+		WorkHours = workHours;
+	}
+	
+	public Worker(int id, String firstName, String lastName, String houseNumber, int zipCode, String street,
+			String phoneNumber, int age, int workHours) {
+		super(id, firstName, lastName, houseNumber, zipCode, street, phoneNumber);
+		Age = age;
+		WorkHours = workHours;
+	}
+}
