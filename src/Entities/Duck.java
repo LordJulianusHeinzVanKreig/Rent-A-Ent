b9f40b153b054e995a70d2c7eb821063c487b1db
VanMeier <meierdo.dm@gmail.com>
2019-04-01 16:31:16 +0200

    Duck class implemented

---
+++ src/Entities/Duck.java
@@ -0,0 +1,49 @@
+package Entities;
+
+public class Duck extends DatabaseEntity {
+	private int Age;
+	private String Name;
+	private String Description;
+	private int Temperament;
+	private boolean Gender;
+	
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public int getTemperament() {
+		return Temperament;
+	}
+	public void setTemperament(int temperament) {
+		Temperament = temperament;
+	}
+	public boolean isGender() {
+		return Gender;
+	}
+	public void setGender(boolean gender) {
+		Gender = gender;
+	}
+	
+	public Duck(int id, int age, String name, String description, int temperament, boolean gender) {
+		super(id);
+		Age = age;
+		Name = name;
+		Description = description;
+		Temperament = temperament;
+		Gender = gender;
+	}
+}
