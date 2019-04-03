6db7a71db25c7c32929c6b404cd61d26f78c77e0
LordJulianusHeinzVanKreig <julian.heinz.98@gmail.com>
2019-04-03 13:28:51 +0200

    entered model functions and parameters

--- src/application/Model/EntenModel.java
+++ src/application/Model/EntenModel.java
@@ -1,5 +1,40 @@
 package application.Model;
 
-public class EntenModel {
+import application.entities.Location;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class EntenModel  {
+	
+	
+	@FXML
+	ListView<Location> listLocations;
+	@FXML
+	ListView listDucks;
+	@FXML
+	ListView listWorkers;
+	
+	@FXML
+	public void handleOnClickListLocations(MouseEvent event) {
+		
+		System.out.println(listLocations.getSelectionModel().getSelectedItem().getStreet());
+	}
+	
+	@FXML
+	public void handleOnClickListDucks() {
+		System.out.println("check");
+	}
+	
+	@FXML 
+	public void handleOnClickListWorkers() {
+		
+	}
+	
+	@FXML
+	public void handleButton(ActionEvent event) {
+		listLocations.getItems().add(new Location(1,"hi",2, "hi2"));
+	}
 	
 }
