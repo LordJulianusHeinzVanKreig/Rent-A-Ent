4979f5f6d5f87af38fcdb8e28d98af37a1678459
LordJulianusHeinzVanKreig <julian.heinz.98@gmail.com>
2019-04-03 14:15:49 +0200

    added Cell Factories

--- src/application/Model/EntenModel.java
+++ src/application/Model/EntenModel.java
@@ -1,10 +1,13 @@
 package application.Model;
 
+import application.Model.cellFactories.LocationViewCell;
 import application.entities.Location;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
 public class EntenModel  {
 	
@@ -35,6 +38,12 @@ public class EntenModel  {
 	@FXML
 	public void handleButton(ActionEvent event) {
 		listLocations.getItems().add(new Location(1,"hi",2, "hi2"));
+		listLocations.setCellFactory(new Callback<ListView<Location>,ListCell<Location>>(){
+			@Override
+			public ListCell<Location> call(ListView<Location> listLocations){
+				return new LocationViewCell();
+			}
+		});
 	}
 	
 }
---
+++ src/application/Model/cellFactories/WorkerViewCell.java
@@ -0,0 +1,15 @@
+package application.Model.cellFactories;
+
+import application.entities.Worker;
+import javafx.scene.control.ListCell;
+
+public class WorkerViewCell extends ListCell<Worker> {
+	
+	@Override
+	public void updateItem(Worker worker, boolean empty) {
+		super.updateItem(worker, empty);
+		if(!empty) {
+			setText(worker.getStreet() +"");	
+		}
+	}
+}
