4eead94ee2af4ac626191d870ed9d6ade10733d8
LordJulianusHeinzVanKreig <julian.heinz.98@gmail.com>
2019-04-04 16:33:14 +0200

    push it to the limit

--- src/application/Model/DuckDetailModel.java
+++ src/application/Model/DuckDetailModel.java
@@ -1,29 +1,42 @@
 package application.Model;
 
 import application.database.entities.Duck;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class DuckDetailModel {
 	
+	@FXML
 	Text DuckDetailName;
+	@FXML
 	Text DuckDetailAge;
+	@FXML
 	Text DuckDetailGender;
+	@FXML
 	Text DuckDetailTemperament;
-	Text DuckDetailDescription;
-	Text DuckDetailType;
+	@FXML
+	TextArea DuckDetailType;
+	@FXML
+	TextArea DuckDetailDescription;
 	
-	public DuckDetailModel(Duck duck) {
-		this.DuckDetailName.setText(duck.getName());
-		this.DuckDetailAge.setText(Integer.toString(duck.getAge()));
-		if(duck.isGender()) this.DuckDetailGender.setText("Männlich");
-		else this.DuckDetailGender.setText("Weiblich");
-		this.DuckDetailTemperament.setText(Integer.toString(duck.getTemperament()));
-		
-	}
+//	public DuckDetailModel(Duck duck) {
+////		
+////		this.DuckDetailName.setText(duck.getName());
+////		this.DuckDetailAge.setText(Integer.toString(duck.getAge()));
+////		if(duck.isGender()) this.DuckDetailGender.setText("Männlich");
+////		else this.DuckDetailGender.setText("Weiblich");
+////		this.DuckDetailTemperament.setText(Integer.toString(duck.getTemperament()));
+////		this.DuckDetailType.setText("Region:" + duck.getType().getRegion() + "\n");
+////		this.DuckDetailType.setText("Brutzeit:" + duck.getType().getBreedTime() + "\n");
+////		this.DuckDetailType.setText("Max. Alter:" + duck.getType().getMaxAge() + "\n");
+////		this.DuckDetailDescription.setText(duck.getDescription());
+//		
+//	}
 	
 	public void startDuckDetailStage() {
 		Stage DuckDetailStage = new Stage();
