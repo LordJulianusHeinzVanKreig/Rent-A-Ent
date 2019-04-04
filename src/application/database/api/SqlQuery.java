c910db430c24f64ee649d8ff592dc58d3ae76e57
VanMeier <meierdo.dm@gmail.com>
2019-04-04 12:46:42 +0200

    Added SQL_selectByIdWithProperties

--- src/application/database/api/SqlQuery.java
+++ src/application/database/api/SqlQuery.java
@@ -45,6 +45,27 @@ public class SqlQuery {
 		return rs;		
 	}
 	
+	public static ResultSet SQL_selectByIdWithProperties(String tabelle, int id, List<String> properties) {
+		ResultSet rs = null;
+		
+		try 
+		{
+			String props = "";
+			for (int i = 0; i < properties.size() - 1; i++) {
+				props += properties.get(i) + ", ";
+			}			
+			String abfrage = "SELECT " + props + " FROM " + SqlConnector.Database + "." + tabelle + " where ID = " + id + ";";
+			rs = SqlConnector.Connection.createStatement().executeQuery(abfrage);
+			
+		}
+		catch (SQLException ex) 
+		{
+			Logger.getLogger(SqlQuery.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+		return rs;		
+	}
+	
 	public static ResultSet SQL_selectProperties(String tabelle, List<String> properties)
 	{
 		ResultSet rs = null;
